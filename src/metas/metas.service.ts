05380e4d8aa9962489ca9d2a99123cca3b5a5c8d
chotaegyu <chotg5592@gmail.com>
2024-05-05 18:02:53 +0900

    FEAT: meta tag findone 추가

--- src/metas/metas.service.ts
+++ src/metas/metas.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateMetaDto } from './dto/create-meta.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { MetaEntity } from '@/metas/entities/meta.entity';
@@ -11,6 +11,18 @@ export class MetasService {
     @InjectRepository(MetaEntity)
     private metasRepository: Repository<MetaEntity>,
   ) {}
+
+  async findOne(id: number) {
+    const metaTag = await this.metasRepository.findOne({
+      where: { id },
+    });
+
+    if (!metaTag) {
+      throw new NotFoundException('해당 Meta Tag 를 찾을 수 없습니다.');
+    }
+
+    return metaTag;
+  }
 
   async createMetaTag(page: PageEntity, metaTag: CreateMetaDto) {
     const { property, content } = metaTag;
