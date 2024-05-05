61b4c6b89a4203ddf87bf6eb7bb92aa3495b9cfd
chotaegyu <chotg5592@gmail.com>
2024-05-05 22:15:56 +0900

    FEAT: cors 세팅

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: ['http://localhost:3000', 'https://ddal-kkak.vercel.app'],
+      credentials: true,
+    },
+  });
 
   const config = new DocumentBuilder()
     .setTitle('DDal-KKak')
