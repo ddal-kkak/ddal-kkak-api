e8b09e88e08d8caa0f033a860fe8ef354867c13b
chotaegyu <chotg5592@gmail.com>
2024-05-05 14:31:42 +0900

    FEAT: postgresql 연동 및 typeorm 기본 세팅 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PagesModule } from './pages/pages.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      entities: [],
+      synchronize: true,
+    }),
+    PagesModule,
+    CommonModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
