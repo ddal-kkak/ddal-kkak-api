a960240fd641d50e543ad64f4e22bf0d05e81b59
chotaegyu <chotg5592@gmail.com>
2024-05-05 14:44:39 +0900

    FEAT: meta 랑 page 랑 관계 설정

--- src/pages/entities/page.entity.ts
+++ src/pages/entities/page.entity.ts
@@ -1,8 +1,34 @@
 import { BaseEntity } from '@/common/entities/base.entity';
+import { Column, Entity, OneToMany } from 'typeorm';
+import { MetaEntity } from '@/metas/entities/meta.entity';
 
-export class Page extends BaseEntity {
+enum PageStatus {
+  DRAFT = 'DRAFT',
+  PUBLISHED = 'PUBLISHED',
+}
+
+@Entity({ name: 'pages' })
+export class PageEntity extends BaseEntity {
+  @Column({
+    length: 20,
+  })
   title: string;
+  @Column({
+    length: 20,
+    unique: true,
+  })
   slug: string;
-  status: 'DRAFT' | 'PUBLISHED';
+  @Column({
+    enum: Object.values(PageStatus),
+    default: PageStatus.DRAFT,
+  })
+  status: PageStatus;
+  @Column({
+    type: 'jsonb',
+    default: {},
+  })
   uiJson: Record<string, any>;
+
+  @OneToMany(() => MetaEntity, (meta) => meta.page)
+  metas: MetaEntity[];
 }
