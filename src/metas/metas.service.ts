3f44516787ba5ac1e031c4396354fc76b248cf24
chotaegyu <chotg5592@gmail.com>
2024-05-05 16:51:19 +0900

    CHORE: MetaTag 에서 불필요한 코드 삭제

--- src/metas/metas.service.ts
+++ src/metas/metas.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { CreateMetaDto } from './dto/create-meta.dto';
-import { UpdateMetaDto } from './dto/update-meta.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { MetaEntity } from '@/metas/entities/meta.entity';
 import { Repository } from 'typeorm';
@@ -13,10 +12,6 @@ export class MetasService {
     private metasRepository: Repository<MetaEntity>,
   ) {}
 
-  create(createMetaDto: CreateMetaDto) {
-    return 'This action adds a new meta';
-  }
-
   async createMetaTag(page: PageEntity, metaTag: CreateMetaDto) {
     const { property, content } = metaTag;
     const meta = this.metasRepository.create({
@@ -26,21 +21,5 @@ export class MetasService {
     });
 
     return this.metasRepository.save(meta);
-  }
-
-  findAll() {
-    return `This action returns all metas`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} meta`;
-  }
-
-  update(id: number, updateMetaDto: UpdateMetaDto) {
-    return `This action updates a #${id} meta`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} meta`;
   }
 }
