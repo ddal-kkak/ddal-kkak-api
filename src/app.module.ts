c0b2838b69ca6fece04eaa473bfbe5d968a6c96d
chotaegyu <chotg5592@gmail.com>
2024-05-05 22:03:41 +0900

    FEAT: DB 정보 env 로 변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,16 +5,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PagesModule } from './pages/pages.module';
 import { CommonModule } from './common/common.module';
 import { MetasModule } from './metas/metas.module';
+import { ConfigModule } from '@nestjs/config';
+import * as process from 'node:process';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
